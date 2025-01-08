5edc3d743d6a2f36c19eb39ef87a6cfb270cd7de
Zeus L <zeuslima360@hotmail.com>
2025-01-08 11:53:09 -0300

    a44

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,24 @@ class AuthController extends Controller
             return view('login');
         }
 
-        public function logout(){
+        public function logout()
+        {
             echo 'logout';
+        }
+
+        public function loginSubmit(Request $request)
+        {
+            //form validation
+            $request->validate(
+                [
+                    'text_username' => 'required',
+                    'text_password' => 'required'
+                ]
+                );
+
+            //echo 'login submit';
+            //dd($request);
+            //echo "<br>";
+            //echo $request->input('text_username');
         }
 }
